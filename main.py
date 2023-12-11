3bc55df0decc507db466616273981ff1eb7d55da
Saulius Serdikevicius <sauliukaser@gmail.com>
2023-12-11 17:06:02 +0200

    Rename, exe file fix

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import pandas
 
 import os
 ROOT_DIR = os.getcwd()
-print(ROOT_DIR)
+
 
 def district_choice(district):
     global user_district_name
@@ -19,14 +19,14 @@ def district_choice(district):
         mark_location = districts_and_cities[district][city]["loc"]
         marking.dot_mark(mark_location)
         district_city_list.append(city)
-    window.destroy()
-    window.quit()
+    window_main.destroy()
+    window_main.quit()
 
 def all_cities():
     if len(user_named_cities) == len(districts_and_cities[user_district_name]):
         messagebox.showinfo(message="Įvardijote visus apskrities miestus!")
-        window_2.destroy()
-        window_2.quit()
+        window_secondary.destroy()
+        window_secondary.quit()
 
 def user_city_mark():
     user_city = entry.get().title()
@@ -50,15 +50,15 @@ def exit_and_save():
             district_city_list.remove(city)
         mokytis = pandas.DataFrame(district_city_list)
         mokytis.to_csv(f"{user_district_name}_apskirtis_neivardinti.txt")
-        window_2.destroy()
-        window.quit()
+        window_secondary.destroy()
+        window_main.quit()
     elif answer == False:
-        window_2.destroy()
-        window.quit()
+        window_secondary.destroy()
+        window_main.quit()
 def off():
     global is_on
     is_on = False
-    window.destroy()
+    window_main.destroy()
     screen.bye()
 
 is_on = True
@@ -71,60 +71,60 @@ while is_on:
     screen = turtle.Screen()
     screen.clear()
     screen.title("Lietuvos žemėlapis")
-    image = ROOT_DIR + "\\dist\\images\\blank_map.gif"
-    # image = "images/blank_map.gif"
+    image = ROOT_DIR +"\\images\\blank_map.gif"
     screen.addshape(image)
     turtle.shape(image)
     screen.setup(1350,1000)
     screen.tracer(0)
+
     marking = Marking()
 
-    window = Tk()
-    window.title("Lietuvos apskirtys")
-    window.geometry("800x800")
+    window_main = Tk()
+    window_main.title("Lietuvos apskirtys")
+    window_main.geometry("800x800")
 
-    separator = ttk.Separator(window)
+    separator = ttk.Separator(window_main)
     separator.pack(fill="x",pady=5)
 
-    label = Label(window, text="Pasirinkite apskritį", anchor="center", font=("Arial", 30, "bold"))
+    label = Label(window_main, text="Pasirinkite apskritį", anchor="center", font=("Arial", 30, "bold"))
     label.pack()
 
-    separator2 = ttk.Separator(window)
+    separator2 = ttk.Separator(window_main)
     separator2.pack(fill="x",pady=5)
 
     districts = ["Šiaulių", "Alytaus", "Utenos", "Kauno", "Vilniaus", "Panevėžio", "Klaipėdos", "Marijampolės", "Tauragės", "Telšių"]
     for district in districts:
-        button = Button(window, borderwidth=3, text=district, width=12, font=("Arial", 25), command=lambda x=district: district_choice(x))
+        button = Button(window_main, borderwidth=3, text=district, width=12, font=("Arial", 25), command=lambda x=district: district_choice(x))
         button.pack()
 
-    button_off = Button(window, text="Išjungti", width=5, font=15, command=off, fg="red")
+    button_off = Button(window_main, text="Išjungti", width=5, font=15, command=off, fg="red")
     button_off.place(x=740, y=760)
-    window.mainloop()
+    window_main.mainloop()
 
-    window_2 = Tk()
-    window_2.title("Miestų įvestis")
-    window_2.geometry("500x500")
-    window_2.bind("<Return>", lambda x: user_city_mark())
+    window_secondary = Tk()
+    window_secondary.title("Miestų įvestis")
+    window_secondary.geometry("500x500")
+    window_secondary.bind("<Return>", lambda x: user_city_mark())
 
-    separator2 = ttk.Separator(window_2)
+    separator2 = ttk.Separator(window_secondary)
     separator2.pack(fill="x", pady=5)
 
-    label_2 = Label(window_2, text=f"Įveskite {user_district_name} apskrities miestą", font=("Arial", 20))
+    label_2 = Label(window_secondary, text=f"Įveskite {user_district_name} apskrities miestą", font=("Arial", 20))
     label_2.pack(pady=5)
 
-    entry = Entry(window_2, borderwidth=3, font=("Arial", 20))
+    entry = Entry(window_secondary, borderwidth=3, font=("Arial", 20))
     entry.pack()
 
-    button_w2 = Button(window_2, borderwidth=3, text="Įvesti", command=user_city_mark, font=("Arial", 20))
-    button_w2.pack()
+    button_insert = Button(window_secondary, borderwidth=3, text="Įvesti", command=user_city_mark, font=("Arial", 20))
+    button_insert.pack()
 
-    button_w2_2 = Button(window_2, borderwidth=3, text="Išjungti", font=("Arial", 20), command=exit_and_save)
-    button_w2_2.pack(side=BOTTOM)
+    button_exit = Button(window_secondary, borderwidth=3, text="Išjungti", font=("Arial", 20), command=exit_and_save)
+    button_exit.pack(side=BOTTOM)
 
-    list_box = Listbox(window_2, borderwidth=3, height=20, width=40, font=15, activestyle="none")
+    list_box = Listbox(window_secondary, borderwidth=3, height=20, width=40, font=15, activestyle="none")
     list_box.pack()
 
     if is_on == False:
-        window_2.destroy()
+        window_secondary.destroy()
 
-    window_2.mainloop()
+    window_secondary.mainloop()
